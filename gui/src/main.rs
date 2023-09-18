2ec0bc3cd59793577775272add9485c82a588930
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2023-09-18 13:18:21 +0200

    Load the lang combo

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -142,7 +142,8 @@ impl SimpleComponent for App {
                 },
                 gtk::Box {
                     set_orientation: gtk::Orientation::Vertical,
-                    gtk::ComboBoxText {},
+                    #[local_ref]
+                    lang_combo -> gtk::ComboBoxText {},
                     gtk::Button {
                         set_label: "Load model",
                         connect_clicked[sender] => move |_| {
@@ -194,24 +195,8 @@ impl SimpleComponent for App {
             lang_combo,
         };
 
+        let lang_combo = model.lang_combo.widget();
         let widgets = view_output!();
-
-        // for lang in all_langs() {
-        //     widgets.lang_combo_box.append_text(lang);
-        //     // TODO set default as active
-        // }
-        //     ui.lang_combo_box.connect_changed({
-        //         let recognition = self.recognition.clone();
-        //         move |combo| {
-        //             let lang = combo.active_text().expect("should be selected");
-        //             recognition
-        //                 .lock()
-        //                 .unwrap()
-        //                 .as_mut()
-        //                 .expect("model not initialized")
-        //                 .set_lang(lang.as_str());
-        //         }
-        //     });
 
         ComponentParts { model, widgets }
     }
@@ -319,44 +304,4 @@ impl App {
 //         }
 //     });
 
-//     let clipboard = Clipboard::get(&gdk::SELECTION_CLIPBOARD);
-//     ui.copy_button.connect_clicked({
-//         let ui = ui.clone();
-//         move |_| {
-//             let buffer = ui.text_view.buffer().expect("textview buffer");
-//             let text = buffer
-//                 .text(&buffer.start_iter(), &buffer.end_iter(), true)
-//                 .expect("buffer text");
-//             clipboard.set_text(text.as_str());
-//         }
-//     });
 
-//     ui.model_choice_button.connect_clicked({
-//         let recognition = self.recognition.clone();
-//         let ui = ui.clone();
-//         move |_| {
-//             let dialog = FileChooserDialog::new(
-//                 Some("Open model"),
-//                 Some(&ui.window),
-//                 gtk::FileChooserAction::Open,
-//             );
-//             dialog.add_button("OK", ResponseType::Accept);
-//             dialog.add_button("Cancel", ResponseType::Cancel);
-
-//             let resp = dialog.run();
-//             dialog.close(); // FIXME: the dialog is not really closed in case of an error. Perhaps idle_add or sth?
-//             if let ResponseType::Accept = resp {
-//                 let model = dialog
-//                     .filename()
-//                     .expect("TODO: when can the filename be none?");
-//                 let mut guard = recognition.lock().unwrap();
-//                 Self::set_model(&mut *guard, ui.as_ref(), &model);
-//             }
-//         }
-//     });
-
-//     // Present window
-//     ui.window.show_all();
-//     // TODO the executable should terminate when the window is closed
-// }
-// }
