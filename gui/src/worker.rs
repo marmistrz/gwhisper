34441d4433b2e6e47465d1790b155044dfbf5b73
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2023-10-06 18:34:07 +0200

    Initialize Recognition inside of the worker (#7)

--- gui/src/worker.rs
+++ gui/src/worker.rs
@@ -14,11 +14,14 @@ pub(crate) enum RecognitionMsg {
 }
 
 impl Worker for RecognitionWorker {
-    type Init = Option<Recognition>; // model: TODO: pass a PathBuf, initialization might take a while
-    type Input = RecognitionMsg; // recording: TODO perhaps send it as a reference??
+    type Init = Option<String>;
+    type Input = RecognitionMsg;
     type Output = Result<String, WhisperError>;
 
-    fn init(recognition: Self::Init, _sender: ComponentSender<Self>) -> Self {
+    fn init(model_path: Self::Init, _sender: ComponentSender<Self>) -> Self {
+        // TODO: propagate this error to the GUI
+        let recognition =
+            model_path.map(|path| Recognition::new(&path).expect("Error creating recognition"));
         Self {
             recognition,
             lang: "auto".into(),
